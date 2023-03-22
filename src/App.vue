efbdecb92c8ff19f94207d216d6608a55d22f577
Shi_Sheng <shisheng2333@gmail.com>
2023-03-22 16:12:54 +0000

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,140 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="chat-box">
+    <div class="title">Fish GPT</div>
+    <div class="chat-history">
+      <div v-for="(message, index) in messages" :key="index">
+        <div class="message" :class="{ 'is-me': message.isMe }">
+          <div class="avatar" v-if="!message.isMe">
+            <img src="https://avatars.githubusercontent.com/u/32504382" alt="avatar" />
+          </div>
+          <div class="text">{{ message.text }}</div>
+          <div class="tag" v-if="message.isMe">我</div>
+        </div>
+      </div>
+    </div>
+    <div class="chat-input">
+      <el-input v-model="inputText" placeholder="请输入消息" @keyup.enter="sendMessage" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+document.title = 'Fish GPT';
+
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'ChatBox',
+  setup() {
+    const messages = ref([]);
+    const inputText = ref('');
+
+    function sendMessage() {
+      if (!inputText.value) {
+        return;
+      }
+      const text = inputText.value;
+      const reply = Math.random() < 0.8 ? '你完蛋了。' : '草我';
+      messages.value.push({ text, isMe: true });
+      messages.value.push({ text: reply, isMe: false });
+      inputText.value = '';
+    }
+
+    return {
+      messages,
+      inputText,
+      sendMessage,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.chat-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 0 20px;
 }
+
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.chat-history {
+  width: 100%;
+  max-width: 600px;
+  flex: 1;
+  overflow-y: auto;
+  padding: 20px;
+}
+
+.message {
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  position: relative;
+}
+
+.is-me {
+  flex-direction: row-reverse;
+  background: linear-gradient(to right, #00c6ff, #0072ff);
+  color: #fff;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.avatar img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.text {
+  flex: 1;
+}
+
+.tag {
+  position: absolute;
+  top: -10px;
+  left: -10px;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  background-color: #f0f0f0;
+  font-size: 12px;
+  font-weight: bold;
+  color: #333;
+}
+
+.chat-input {
+  width: 100%;
+  max-width: 600px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.el-input {
+  flex: 1;
+  margin-right: 20px;
+}
+
 </style>
