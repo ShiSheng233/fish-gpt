5884a78b46626cdc90678c4c939542bddd240598
lvzhenbo <294221990@qq.com>
2023-03-23 23:21:51 +0800

    feat: 设置蓝图为MD3，修改AppBar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,10 +1,23 @@
 <template>
   <v-app-bar flat>
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      Bard
+      <v-chip class="ma-2" color="primary" variant="outlined">
+        Experiment
+      </v-chip>
     </v-app-bar-title>
+    <template v-slot:append>
+      <v-btn icon="mdi-apps"></v-btn>
+      <v-btn icon>
+        <v-avatar>
+          <v-icon icon="mdi-account-circle"></v-icon>
+        </v-avatar>
+      </v-btn>
+
+    </template>
   </v-app-bar>
 </template>
 
